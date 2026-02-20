ded2241fb54d3578311a2f63fccc7b165a0bb162
priyanshuANDcoad <priyanshu23154050@akgec.ac.in>
2026-02-20 21:56:38 +0530

    post

--- blog_backend/src/app.js
+++ blog_backend/src/app.js
@@ -1,12 +1,9 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 
 import postRoutes from "./routes/post.routes.js";
 import commentRoutes from "./routes/comment.routes.js";
 import authRoutes from "./routes/auth.routes.js";
-
-dotenv.config();
 
 const app = express();
 
