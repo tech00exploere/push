d6540abf21d342b760911dd2a9ca0da21eb643bf
Priyanshu <priyanshu2507.rjs@gmail.com>
2026-03-03 00:59:07 +0530

    Update server.js

--- blog_backend/src/server.js
+++ blog_backend/src/server.js
@@ -1,21 +1,14 @@
-import path from "path";
-import { fileURLToPath } from "url";
 import dotenv from "dotenv";
-
 import connectDB from "./config/db.js";
 import app from "./app.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+dotenv.config();
 
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
-
-const PORT = Number(process.env.PORT) || 5000;
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
     await connectDB();
-
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
